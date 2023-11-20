cbc05da0d36b2922440a4c5ff9b26353be62c1e9
Daniel Mills <danielm88341@gmail.com>
2023-11-19 19:14:30 -0500

    more updates

---
+++ freecodecamp/palm2api/server.js
@@ -0,0 +1,10 @@
+const PORT = 8000;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
